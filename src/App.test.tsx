cb4264a172640e87d88d73b24ae6393552a136e8
03gibbss <sam-gibbs1@hotmail.co.uk>
2023-09-26 18:46:06 +0100

    feat: implemented basic caeser cipher

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,129 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render the string input, shift amount input and output', () => {
+    render(<App />);
+    const stringInput = screen.getByLabelText('Input string');
+    expect(stringInput).toBeInTheDocument();
+    const shiftAmountInput = screen.getByLabelText('Shift amount');
+    expect(shiftAmountInput).toBeInTheDocument();
+    const stringOutput = screen.getByTestId('string-output');
+    expect(stringOutput).toBeInTheDocument();
+  });
+
+  it('output should match input', () => {
+    render(<App />);
+    const stringInput = screen.getByLabelText('Input string');
+    fireEvent.change(stringInput, {
+      target: {
+        value: 'ABC',
+      },
+    });
+    const stringOutput = screen.getByTestId('string-output');
+    expect(stringOutput).toHaveTextContent('ABC');
+  });
+
+  it('output should be shifted by 1', () => {
+    render(<App />);
+    const stringInput = screen.getByLabelText('Input string');
+    fireEvent.change(stringInput, {
+      target: {
+        value: 'ABC',
+      },
+    });
+    const shiftAmountInput = screen.getByLabelText('Shift amount');
+    fireEvent.change(shiftAmountInput, {
+      target: {
+        value: 1,
+      },
+    });
+    const stringOutput = screen.getByTestId('string-output');
+    expect(stringOutput).toHaveTextContent('BCD');
+  });
+
+  it('output should be shifted by -1', () => {
+    render(<App />);
+    const stringInput = screen.getByLabelText('Input string');
+    fireEvent.change(stringInput, {
+      target: {
+        value: 'BCD',
+      },
+    });
+    const shiftAmountInput = screen.getByLabelText('Shift amount');
+    fireEvent.change(shiftAmountInput, {
+      target: {
+        value: -1,
+      },
+    });
+    const stringOutput = screen.getByTestId('string-output');
+    expect(stringOutput).toHaveTextContent('ABC');
+  });
+
+  it('output should pass through non-letters unaffected', () => {
+    render(<App />);
+    const stringInput = screen.getByLabelText('Input string');
+    fireEvent.change(stringInput, {
+      target: {
+        value: '123;# []!$&"(',
+      },
+    });
+    const stringOutput = screen.getByTestId('string-output');
+    expect(stringOutput).toHaveTextContent('123;# []!$&"(');
+  });
+
+  it('output should pass through non-letters unaffected when shifted by 1', () => {
+    render(<App />);
+    const stringInput = screen.getByLabelText('Input string');
+    fireEvent.change(stringInput, {
+      target: {
+        value: '123A; #[]B!$&"(C',
+      },
+    });
+    const shiftAmountInput = screen.getByLabelText('Shift amount');
+    fireEvent.change(shiftAmountInput, {
+      target: {
+        value: 1,
+      },
+    });
+    const stringOutput = screen.getByTestId('string-output');
+    expect(stringOutput).toHaveTextContent('123B; #[]C!$&"(D');
+  });
+
+  it('output should be shifted by 1 and loop around to the start of the range', () => {
+    render(<App />);
+    const stringInput = screen.getByLabelText('Input string');
+    fireEvent.change(stringInput, {
+      target: {
+        value: 'YZA',
+      },
+    });
+    const shiftAmountInput = screen.getByLabelText('Shift amount');
+    fireEvent.change(shiftAmountInput, {
+      target: {
+        value: 1,
+      },
+    });
+    const stringOutput = screen.getByTestId('string-output');
+    expect(stringOutput).toHaveTextContent('ZAB');
+  });
+
+  it('output should be shifted by -1 and loop around to the end of the range', () => {
+    render(<App />);
+    const stringInput = screen.getByLabelText('Input string');
+    fireEvent.change(stringInput, {
+      target: {
+        value: 'ZAB',
+      },
+    });
+    const shiftAmountInput = screen.getByLabelText('Shift amount');
+    fireEvent.change(shiftAmountInput, {
+      target: {
+        value: -1,
+      },
+    });
+    const stringOutput = screen.getByTestId('string-output');
+    expect(stringOutput).toHaveTextContent('YZA');
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputString, setInputString] = useState('');
+  const [shiftAmount, setShiftAmount] = useState(0);
+
+  const handleInputStringChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    // Only allow uppercase letters
+    const uppercaseValue = value.toUpperCase();
+    setInputString(uppercaseValue);
+  };
+
+  const inputStringAsArray = Array.from(inputString);
+  const outputCharCodeArray = inputStringAsArray.map((character) => {
+    const charCode = character.charCodeAt(0);
+    if (!/^[A-Z]$/.test(character)) {
+      return charCode;
+    }
+    return shift(charCode);
+  });
+
+  function shift(charCode: number) {
+    const low = 65; // Character code for letter A
+    const high = 90 + 1; // Character code for letter Z + 1 to include Z
+    const range = high - low;
+    // Keep charCode within the range, loop around at the top and bottom
+    return ((charCode - low + (shiftAmount % range) + range) % range) + low;
+  }
+
+  const outputString = String.fromCharCode(...outputCharCodeArray);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Caeser cipher</h1>
+      <div>
+        <label htmlFor="input-string">Input string</label>
+        <input value={inputString} onChange={handleInputStringChange} id="input-string" />
+      </div>
+      <div>
+        <label htmlFor="shift-amount">Shift amount</label>
+        <input
+          type="number"
+          min={-25}
+          max={25}
+          id="shift-amount"
+          value={shiftAmount}
+          onChange={(event) => setShiftAmount(event.target.valueAsNumber)}
+        />
+        <p>(Values between -25 and 25)</p>
+      </div>
+      <div>
+        <p data-testid="string-output">{outputString}</p>
+      </div>
+    </section>
   );
 }
 
